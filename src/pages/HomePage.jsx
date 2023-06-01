7cf049b11bc280c407f5ad6bde3ad71adcf0891b
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-01 14:10:21 +0200

    fix: suppression state.count non importé

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { context } from "./../reduces/provider";
-import Button from "./../components/Button";
 import State from "../models/State";
 
 const HomePage = () => {
@@ -10,10 +9,6 @@ const HomePage = () => {
   const [state, dispatch] = useContext(context);
   return (
     <>
-      <p style={{ color: "white", font: "bold" }}>
-        propriété count de contexte : {state.count}
-      </p>
-      <Button />
       <br />
       {state.articles.map((article) => (
         <article style={{ display: "flex", flexWrap: "wrap" }}>
