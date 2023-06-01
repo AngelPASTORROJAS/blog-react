ed69c5f452bb33931586dbf6b2d254b5847e0ba0
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-01 11:26:01 +0200

    feat: ajout des articles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,33 @@
 import { useContext } from "react";
 import { context } from "./../reduces/provider";
-import Button from "./../components/Button"
+import Button from "./../components/Button";
 
 const HomePage = () => {
   const [state, dispatch] = useContext(context);
   return (
     <>
-      <p style={{color:"white", font:"bold"}}>propriété count de contexte : {state.count}</p>
+      <p style={{ color: "white", font: "bold" }}>
+        propriété count de contexte : {state.count}
+      </p>
       <Button />
+      <br />
+      {state.articles.map((article) => (
+        <article style={{ display: "flex", flexWrap: "wrap" }}>
+          <img
+            style={{ margin: "10px" }}
+            width="300px"
+            src={article.image}
+            alt=""
+          />
+          <div>
+            <h2 style={{ margin: "10px", color: "red" }}>{article.titre}</h2>
+            <p style={{ margin: "10px", color: "red" }}>
+              {article.description}
+            </p>
+            <p style={{ margin: "10px", color: "red" }}> {article.date} </p>
+          </div>
+        </article>
+      ))}
     </>
   );
 };
--- src/reduces/reducer.js
+++ src/reduces/reducer.js
@@ -1,5 +1,9 @@
 const initialState = {
   count: 0,
+  articles: [
+    { titre: "titre1", image: "https://www.autour-dumonde.fr/sx-content/uploads/cms/img-presentation-1.jpg", description: "desc1", date: "date1" },
+    { titre: "titre2", image: "https://ressources.regionsjob.com/seo/ObsJob/conseiller-voyages.jpg", description: "desc2", date: "date2" }
+  ],
 };
 
 const reducer = (state, action) => {
